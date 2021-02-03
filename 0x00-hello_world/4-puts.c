d0677cde72482d65d565fcfbb1f6468bed4fb752
livejo <yoseph.kefale@gmail.com>
2021-02-03 15:57:25 +0000

    wiz header

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+int main()
+
+{
+
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return 0;
+
+}
