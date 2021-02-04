5a728b990f5420c97d7b4f1ae00ca5b0c8ed1fb8
livejo <yoseph.kefale@gmail.com>
2021-02-04 18:59:49 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - random numbers
+* Return: last digit
+*/
+int main(void)
+{
+for(int x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = tolower(x);
+putchar(lower_x);
+}
+return 0;
+}
