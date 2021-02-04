adc45099b14420918b77d24652e53fe4e69b7c38
livejo <yoseph.kefale@gmail.com>
2021-02-04 20:13:23 +0300

    Create 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - random numbers
+* Return: last digit
+*/
+int main(void)
+{
+for(int i = 0; i < 100 ; i++){
+putchar((i/10)+'0');
+putchar((i%10)+'0');
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
