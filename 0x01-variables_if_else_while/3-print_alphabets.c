4622c740ca78658be38894217c3d3e550e35bd86
livejo <yoseph.kefale@gmail.com>
2021-02-04 19:26:09 +0300

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - random numbers
+* Return: last digit
+*/
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (x= 'A'; x <= 'Z'; x++)
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
