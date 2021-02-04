c30dc2bda825185de73f56f4352094bfd0f11968
livejo <yoseph.kefale@gmail.com>
2021-02-04 17:13:24 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,12 @@
 *
 * Return: Always 0 (Success)
 */
-int main()
- char i;
- for(i='A'; i<='Z'; i++)
- putchar(i);
- for(i='a'; i<='z'; i++)
- putchar(i);
- putchar('\n');
- return 0;
+int main(void)
+char i;
+for (i = 'A'; i <= 'Z'; i++)
+putchar(i);
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return 0;
 }
