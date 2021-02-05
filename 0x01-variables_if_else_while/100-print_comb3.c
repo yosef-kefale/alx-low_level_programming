2c9696291b1e3b4de9b011cca09d116ec6182622
livejo <yoseph.kefale@gmail.com>
2021-02-05 11:53:11 +0000

    chna

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,10 @@ for (x = '0'; x <= '9'; x++)
 {
 for (y = '0'; y <= '9'; y++)
 {
+if (x < y)
+{
 putchar(x);
 putchar(y);
-if (x != 0 && y != 0(
-{
 if (!(x == '8' && y == '9'))
 {
 putchar(',');
