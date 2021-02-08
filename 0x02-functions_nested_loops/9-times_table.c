ed65c0fc46d30a9074eea6de45ded7e1d30f1dd7
livejo <yoseph.kefale@gmail.com>
2021-02-09 01:17:32 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,12 @@
 */
 void times_table(void)
 {
-char i;
-char j;
-char y;
-for (i = '0'; i <= '9'; i++)
+int i;
+int j;
+int y;
+for (i = 0; i <= 9; i++)
 {
-for (j = '0'; j <= '9'; j++)
+for (j = 0; j <= 9; j++)
 {
 y = i * j;
 _putchar(y);
