c71828351f3eb4afd479e0fb595333d09d481760
livejo <yoseph.kefale@gmail.com>
2021-02-09 00:58:21 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 if (n > 0)
 {
 return (1);
-_putchar('+');
+_putchar(+);
 }
 else if (n == 0)
 {
 return (0);
-_putchar('0');
+_putchar(0);
 }
 if (n < 0)
 {
 return (-1);
-_putchar('-');
+_putchar(-);
 }
 return (0);
 }
