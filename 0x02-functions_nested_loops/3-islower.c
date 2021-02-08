4ca610986b3da9b39fa657b906fae428360d2100
livejo <yoseph.kefale@gmail.com>
2021-02-08 15:39:38 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+int _islower(int c)
+{
+if(c >= 'a' && c <= 'z')
+{
+return (1);;
+}
+else if(c >= 'A' && c <= 'Z')
+{
+return (0);
+}
+}
+int main(void)
+{
+int _islower();
+return (0);
+}
