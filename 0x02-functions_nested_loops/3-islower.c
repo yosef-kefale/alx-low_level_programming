9296d82dcea24bf8c153f9278fc581c0de9b4219
livejo <yoseph.kefale@gmail.com>
2021-02-08 15:42:09 +0000

    all

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,7 +11,7 @@ if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
-else if (c >= 'A' && c <= 'Z')
+else
 {
 return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "Holberton.h"
+#include "holberton.h"
 /**
 * _isalpha - does my stuff
 * @c: user input
