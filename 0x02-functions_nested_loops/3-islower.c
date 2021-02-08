f7521032731724cf254b2c10b2221598da2fbcd9
livejo <yoseph.kefale@gmail.com>
2021-02-08 12:52:48 +0000

    new all

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,17 +6,24 @@
 */
 int _islower(int c)
 {
-if(c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
-return (1);;
+return (1);
 }
-else if(c >= 'A' && c <= 'Z')
+else if (c >= 'A' && c <= 'Z')
 {
 return (0);
 }
 }
 int main(void)
 {
-int _islower();
+int r;
+r = _islower('H');
+_putchar(r + '0');
+r = _islower('o');
+_putchar(r + '0');
+r = _islower(108);
+_putchar(r + '0');
+_putchar('\n');
 return (0);
 }
