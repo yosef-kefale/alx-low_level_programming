8be0eb473fd38389189d1281f4f3ba7ee8d07a18
livejo <yoseph.kefale@gmail.com>
2021-02-08 12:30:12 +0000

    alpha x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@ void print_alphabet(void)
 {
 char ch;
 int i;
-i=0;
-while(i<10)
+i = 0;
+while (i < 10)
 {
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 _putchar(ch);
+}
+_putchar('\n'); 
 i++;
 }
-}
-_putchar('\n');
 }
 int main(void)
 {
