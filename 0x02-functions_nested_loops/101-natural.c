ef1a85916a27b1ab4aeac1ed485b503dd0992706
livejo <yoseph.kefale@gmail.com>
2021-02-09 11:36:11 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /**
-* main - does my stuff
+* print_times_table - does my stuff
 * print the alphabet
 *
-* ten times
+* @n: times
 */
-int main()
+void print_times_table(int n){
+if (n < 15 && n > 0)
 {
-int n = 1024;
-int remain3;
-int remain5;
-int sum;
-sum = 0;
-for (int i = 0; i < n; i++)
+for (int i = 0; i <= n; i++)
 {
-remain5 = i % 5;
-remain3 = i % 3;
-if (remain3 == 0 || remain5 == 0)
+for (int j = 0; j <= n; j++){
+int table = i * j;
+if (j != n)
 {
-sum = sum + i;
+printf("%d, ", table);
+}
+else
+printf("%d", table);
+}
+printf("\n");
 }
 }
-printf("%d\n", sum);
 }
