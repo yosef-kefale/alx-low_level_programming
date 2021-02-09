8b04e38fb02ae2272cf693967f8230c0dee917ed
livejo <yoseph.kefale@gmail.com>
2021-02-09 16:07:25 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,30 +8,18 @@
 void jack_bauer(void)
 {
 int HourTen = '0';
-int HourOne = '0';
-int MinuteTen = '0';
-int Minuteone = '0';
-for (HourTen = '0'; HourTen <= '9'; HourTen++)
+int Minute = '0';
+for (HourTen = '0'; HourTen <= '23'; HourTen++)
 {
-for (HourOne = '0'; HourOne <= '9'; HourOne++)
+for (Minute = '0'; Minute <= '59'; Minute++)
 {
-for (MinuteTen = '0'; MinuteTen <= '5'; MinuteTen++)
-{
-for (Minuteone = '0'; Minuteone <= '9'; Minuteone++)
-{
-if (MinuteTen == 5 && Minuteone == 9)
-break;
-else
-{
-_putchar (HourTen);
-_putchar (HourOne);
-_putchar (':');
-_putchar (MinuteTen);
-_putchar (Minuteone);
+
+_putchar('0' + (HourTen / 10));
+_putchar('0' + (HourTen % 10));
+_putchar(':');
+_putchar('0' + (Minute / 10));
+_putchar('0' + (Minute % 10));
 _putchar ('\n');
-}
-}
-}
 }
 }
 _putchar('\n');
