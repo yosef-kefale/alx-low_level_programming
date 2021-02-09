f01bd63a8ddd5ced69958eff2421d515edd8b3b1
livejo <yoseph.kefale@gmail.com>
2021-02-09 11:52:15 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - random numbers
+* last digit
+*/
+int main()
+{
+int n = 1024;
+int remain3;
+int remain5;
+int sum;
+sum = 0;
+for (int i = 0; i < n; i++)
+{
+remain5 = i % 5;
+remain3 = i % 3;
+if (remain3 == 0 || remain5 == 0)
+{
+sum = sum + i;
+}
+}
+printf("%d",sum);
+}
