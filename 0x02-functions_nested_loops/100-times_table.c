a0024788d0fed39081d88def60f4b50f83b80b4e
livejo <yoseph.kefale@gmail.com>
2021-02-09 23:35:06 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,7 @@ void print_times_table(int n)
 int row;
 int column;
 int product;
+int remain100;
 if (n < 15 && n > 0)
 {
 for (row = 0; row <= n; row++)
@@ -17,6 +18,7 @@ for (row = 0; row <= n; row++)
 for (column = 0; column <= n; column++)
 {
 product = (row * column);
+remain100 = (product % 100);
 if (column == 0)
 {
 _putchar('0' + product);
@@ -40,7 +42,7 @@ else if (product > 99)
 _putchar(',');
 _putchar(' ');
 _putchar('0' + (product / 100));
-_putchar('0' + (product / 10));
+_putchar('0' + (remain100 / 10));
 _putchar('0' + (product % 10));
 }
 }
