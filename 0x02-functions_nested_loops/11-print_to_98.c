b06b786bf412f070b83a1979a3749742611ecd0c
livejo <yoseph.kefale@gmail.com>
2021-02-09 11:12:12 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,14 @@ if (n < 98)
 for (i = n; i <= 98; i++)
 {
 if (i != 98)
-printf("%d, ",i);
+printf("%d, ", i);
 else
-printf("%d",i);
+printf("%d", i);
 }
 printf("\n");
 }
 else if (n == 98){
-printf("%d",n);
+printf("%d", n);
 printf("\n");
 }
 else if (n > 98)
@@ -29,10 +29,10 @@ else if (n > 98)
 for(j = n;j >= 98; j--)
 {
 if (j != 98){
-printf("%d, ",j);
+printf("%d, ", j);
 }
 else
-printf("%d",j);
+printf("%d", j);
 }
 printf("\n");
 }
