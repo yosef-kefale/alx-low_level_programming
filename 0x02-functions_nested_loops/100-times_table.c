8c8ec74ec9cf6aad46d5fc8dee30b66ab45a5156
livejo <yoseph.kefale@gmail.com>
2021-02-09 11:40:33 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,12 +5,14 @@
 *
 * @n: times
 */
-void print_times_table(int n){
+void print_times_table(int n)
+{
 if (n < 15 && n > 0)
 {
 for (int i = 0; i <= n; i++)
 {
-for (int j = 0; j <= n; j++){
+for (int j = 0; j <= n; j++)
+{
 int table = i * j;
 if (j != n)
 {
