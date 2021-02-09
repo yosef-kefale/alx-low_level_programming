a0265fb18dabb282f14ba43a4c40585ca9f6d01f
livejo <yoseph.kefale@gmail.com>
2021-02-09 19:57:51 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,10 @@ for (i = '0'; i <= '9'; i++)
 for (j = '0'; j <= '9'; j++)
 {
 r = i * j;
-_putchar('0' + r);
+
+_putchar('0' + (r / 10));
+_putchar('0' + (r % 10));
 _putchar(',');
-_putchar(' ');
 }
 _putchar('\n');
 }
