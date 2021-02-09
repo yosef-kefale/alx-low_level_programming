fc63b6bab34df89730196b891632ece2a8b1d079
livejo <yoseph.kefale@gmail.com>
2021-02-09 10:48:14 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,24 +5,25 @@
 *
 * ten times
 */
-void dividend(){
+void dividend(void)
+{
 int n = 1024;
 int remain3;
 int remain5;
 int sum;
 sum = 0;
-for (int i = 0;i<n;i++)
+for (int i = 0; i < n; i++)
 {
-remain5= i % 5;
-remain3= i % 3;
+remain5 = i % 5;
+remain3 = i % 3;
 if (remain3 == 0 || remain5 == 0)
 {
 sum = sum + i;
 }
 }
-printf("%d\n",sum);
+printf("%d\n", sum);
 }
-int main()
+int main(void)
 {
 dividend();
 }
