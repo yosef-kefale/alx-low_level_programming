baf3dd218bf438ba88532dbdff1456ef3e79f200
livejo <yoseph.kefale@gmail.com>
2021-02-10 19:38:04 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,22 +5,20 @@
 */
 void printFibonacciNumbers(int n)
 {
-    int f1 = 0, f2 = 1, i;
- 
-    if (n < 1)
-        return;
-    printf("%d ", f1);
-    for (i = 1; i < n; i++) {
-        printf("%d ", f2);
-        int next = f1 + f2;
-        f1 = f2;
-        f2 = next;
-    }
+int f1 = 0, f2 = 1, i;
+if (n < 1)
+return;
+printf("%d ", f1);
+for (i = 1; i < n; i++)
+{
+printf("%d ", f2);
+int next = f1 + f2;
+f1 = f2;
+f2 = next;
 }
- 
-// Driver Code
+} 
 int main()
 {
-    printFibonacciNumbers(7);
-    return 0;
+printFibonacciNumbers(7);
+return (0);
 }
