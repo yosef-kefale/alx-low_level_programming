a65670dacb5a8357254f0861a9c04e2107a4f010
livejo <yoseph.kefale@gmail.com>
2021-02-10 10:30:06 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,14 @@ int print_last_digit(int n)
 {
 int Last;
 Last = n % 10;
-if(Last >= 0)
+if (Last >= 0)
 {
 _putchar(Last + '0');
 }
 else
 {
 Last = Last * -1;
-_putchar(Last + '0');   
+_putchar(Last + '0');
 }
 return (Last);
 }
