7e451a0b4c510c63e0195b935f5909b0ce0997e6
livejo <yoseph.kefale@gmail.com>
2021-02-10 20:40:42 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,25 +8,24 @@ void printFibonacciNumbers(int n)
 int f1;
 int f2;
 int i;
+int Fdigit;
 int next;
 f1 = 0;
 f2 = 1;
-if (n < 1)
-return;
-printf("%d, ", f1);
 for (i = 1; i <= n; i++)
 {
-if (i == 50)
+if (Fdigit == 1 || Fdigit == 1)
 {
-printf("%d\n", f2);
-}
-else
-{
-printf("%d, ", f2);
+printf("%d, ", Fdigit);
 }
 next = f1 + f2;
 f1 = f2;
 f2 = next;
+Fdigit=f2;
+while(Fdigit>=10)
+{
+Fdigit/=10;
+}
 }
 }
 /**
