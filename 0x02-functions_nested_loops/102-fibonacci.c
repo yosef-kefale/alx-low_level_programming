8de2e5555a66c995782185e25a664cdb10d9c105
livejo <yoseph.kefale@gmail.com>
2021-02-10 20:02:35 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,10 +13,12 @@ f1 = 0;
 f2 = 1;
 if (n < 1)
 return;
-printf("%d, ", f1);
+printf("%d", f1);
+printf(", ");
 for (i = 1; i < n; i++)
 {
 printf("%d ", f2);
+printf(", ");
 next = f1 + f2;
 f1 = f2;
 f2 = next;
