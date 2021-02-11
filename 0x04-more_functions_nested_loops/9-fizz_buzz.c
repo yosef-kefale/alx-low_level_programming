6405258e8d4235a3042c7deda3c9ce9e9cded464
livejo <yoseph.kefale@gmail.com>
2021-02-11 20:43:47 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ if ((i % 3) == 0)
 {
 printf("Fizz ");
 }
-if ((i % 5) == 0)
+else if ((i % 5) == 0)
 {
 printf("Buzz ");
 }
-if ((i % 3) == 0 && (i % 5) == 0)
+else if ((i % 3) == 0 && (i % 5) == 0)
 {
 printf("FizzBuzz ");
 }
