f5d2f2697d8caeba696c23d9c6fc2871b42e1605
livejo <yoseph.kefale@gmail.com>
2021-02-11 15:34:18 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,12 +5,12 @@
 * @size: return nothing
 * Return: 1 for digit and 0 for else
 */
-void print_square()
+void print_square(int size)
 {
 int i;
-for(i = 0; i <= 10; i++)
+for(i = 0; i <= size; i++)
 {
-for(i = 0; i <= 10; i++)
+for(i = 0; i <= size; i++)
 {
 _putchar('#');
 }
