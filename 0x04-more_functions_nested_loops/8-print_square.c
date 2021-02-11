33969e3ad8d530146c03034fcf91fea7e4bc51ad
livejo <yoseph.kefale@gmail.com>
2021-02-11 15:25:05 +0300

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* print_square -checks for upper case
+*
+* @size: return nothing
+* Return: 1 for digit and 0 for else
+*/
+void print_square(int size)
+{
+int i;
+for (i = 0;i <= size; i++){
+for (i = 0;i <= size; i++){
+_putchar('#');
+}
+_putchar('\n');
+}
+return (0);
+}
