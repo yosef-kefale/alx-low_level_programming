8efae7df5d72ddbb53d78e5219753b5180bd7e82
livejo <yoseph.kefale@gmail.com>
2021-02-11 17:16:24 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,23 +7,23 @@ int i;
 int max;
 max = -1;
 n = 612852475143;
-while(n % 2 == 0)
+while( n % 2 == 0)
 {
 max = 2;
 n = n / 2;
 }
-for(i = 3; i <= sqrt (n); i = i + 2)
+for (i = 3; i <= sqrt(n); i = i + 2)
 {
-while(n % i == 0)
+while (n % i == 0)
 {
 max = i;
 n = n / i;
 }
 }
-if(n > 2)
+if (n > 2)
 {
 max = n;
 }
-printf("%d",max);
+printf("%d", max);
 return (0);
 }
