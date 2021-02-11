b340ec64f95fc9b52bf7e616a6086f7f8c2428d6
livejo <yoseph.kefale@gmail.com>
2021-02-11 14:40:30 +0300

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_diagonal -checks for upper case
+*
+* @n: return nothing
+* Return: 1 for digit and 0 for else
+*/
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar(' ');
+}
+_putchar('\');
+_putchar('\n');
+}
