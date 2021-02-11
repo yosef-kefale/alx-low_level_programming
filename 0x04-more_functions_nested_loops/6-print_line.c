76444439c97de61fe4ce906970645b53bb6f4638
livejo <yoseph.kefale@gmail.com>
2021-02-11 14:35:33 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_line -checks for upper case
+*
+* @n: return nothing
+* Return: 1 for digit and 0 for else
+*/
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
