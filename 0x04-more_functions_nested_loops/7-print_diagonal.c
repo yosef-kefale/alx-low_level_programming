e87c7207debe2edab39d7a01f9774c2a68d7c6ad
livejo <yoseph.kefale@gmail.com>
2021-02-11 22:31:07 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,14 @@
 void print_diagonal(int n)
 {
 int i;
-for (i = 0; i <= n; i++)
+int j;
+for (i = 0; i < n; i++)
 {
-while (n > 0)
-{
+for (j = 0; j < i; j++) {
 _putchar(' ');
-n--;
 }
 _putchar('\\');
 _putchar('\n');
 }
-_putchar('\n');
+return (0);
 }
