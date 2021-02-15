4102344669d3bfc9bb06c0091df0362c8dde3c93
livejo <yoseph.kefale@gmail.com>
2021-02-15 15:40:29 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* print_rev -change value using the pointer address
+*
+*@s: stores the input
+*/
+void print_rev(char *s)
+{
+int i;
+char temp;
+int length = 0;
+char str[];
+temp = *str;
+str = temp;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+for(i = length - 1; i >= 0; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
