451d370c077553faabb1d5355b209eb57f3bcee1
livejo <yoseph.kefale@gmail.com>
2021-02-15 12:09:35 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _strlen -change value using the pointer address
+*
+*@s: stores the input
+*Return: length
+*/
+int _strlen(char *s)
+{
+char h[] = *s;
+int i;
+for (i = 0; s[i] != '\0'; ++i);  
+return (i);
+}
