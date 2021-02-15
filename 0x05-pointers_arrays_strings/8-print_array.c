3eed507a54eec75bf9f970726920f460920cde26
livejo <yoseph.kefale@gmail.com>
2021-02-16 00:05:31 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,8 +8,8 @@
 */
 void print_array(int *a, int n)
 {
-int i; 
-for(i=0;i<n;i++)
+int i;
+for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
 if (i != (n - 1))
@@ -18,4 +18,4 @@ printf(", ");
 }
 }
 printf("\n");
-} 
+}
