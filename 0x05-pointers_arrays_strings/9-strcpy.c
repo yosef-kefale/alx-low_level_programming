9edfec0c2e896cc13438bb4803ad3c566b84198d
livejo <yoseph.kefale@gmail.com>
2021-02-15 21:58:51 +0300

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _strcpy -copy values from pointer to pointer
+*
+*@dest: stores the input
+*@drc:stores the input
+*/
+char *_strcpy(char *dest, char *src)
+{
+while(*src)
+{
+*dest = *src;        
+src++;        
+dest++;
+}    
+*dest = '\0';
+return (*dest);
+}
