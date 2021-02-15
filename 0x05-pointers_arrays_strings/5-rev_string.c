ead35b2e9846921ea0aabaaf925b75e3ae03b732
livejo <yoseph.kefale@gmail.com>
2021-02-15 17:45:53 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,34 +4,24 @@
 *
 *@s: stores the input
 */
-void rev_string(char *str)
+void rev_string(char *s)
 {
-int l, i; 
-    char *begin_ptr, *end_ptr, ch; 
-  
-    // Get the length of the string 
-    l = strlen(str); 
-  
-    // Set the begin_ptr and end_ptr 
-    // initially to start of string 
-    begin_ptr = str; 
-    end_ptr = str; 
-  
-    // Move the end_ptr to the last character 
-    for (i = 0; i < l - 1; i++) 
-        end_ptr++; 
-  
-    // Swap the char from start and end 
-    // index using begin_ptr and end_ptr 
-    for (i = 0; i < l / 2; i++) { 
-  
-        // swap character 
-        ch = *end_ptr; 
-        *end_ptr = *begin_ptr; 
-        *begin_ptr = ch; 
-  
-        // update pointers positions 
-        begin_ptr++; 
-        end_ptr--; 
-    }  
+int l;
+int i; 
+char *begin_ptr;
+char *end_ptr;
+char ch; 
+l = strlen(s); 
+begin_ptr = s; 
+end_ptr = s; 
+for (i = 0; i < l - 1; i++) 
+end_ptr++;  
+for (i = 0; i < l / 2; i++)
+{
+ch = *end_ptr; 
+*end_ptr = *begin_ptr; 
+*begin_ptr = ch; 
+begin_ptr++; 
+end_ptr--; 
+}  
 } 
