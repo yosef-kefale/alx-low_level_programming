8f51b797280b69009f315b63877abeb954e20c0d
livejo <yoseph.kefale@gmail.com>
2021-02-16 18:05:42 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,20 @@
 */
 void print_rev(char *s)
 {
-    int i, j, k;
-    char *temp = s;
-    char rev[100];
-    for(i = 0; temp[i] != '\0'; i++);
-    {
-        k = i-1;
-    }
-    for(j = 0; j <= i-1; j++)
-    {
-        rev[j] = temp[k];
-        _putchar(rev[j]);
-        k--;
-    }
-    
-  _putchar('\n');
+int i;
+int j;
+int k;
+char *temp = s;
+char rev[100];
+for (i = 0; temp[i] != '\0'; i++);
+{
+k = i-1;
+}
+for(j = 0; j <= i-1; j++)
+{
+rev[j] = temp[k];
+_putchar(rev[j]);
+k--;
+}
+_putchar('\n');
 }
