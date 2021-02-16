1c1e01ed32e5138b6c7c10cd570375a6bfb8cf5b
livejo <yoseph.kefale@gmail.com>
2021-02-16 17:45:56 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,16 @@ void rev_string(char *s)
 int i;
 int halflength;
 int last_pos;
-char *temp=s;
+char *temp = s;
 int length = 0;
 while (*temp != '\0')
 {
 length++;
 temp++;
 }
-last_pos = length-1;
+last_pos = length - 1;
 halflength = length / 2;
-for(i = 0; i < halflength; i++)
+for (i = 0; i < halflength; i++)
 {
 char tmp = s[i];
 s[i] = s[last_pos - i];
