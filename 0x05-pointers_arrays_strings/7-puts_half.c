2dc110c2a0a1df00f8bd84c5affb8bea1ad85045
livejo <yoseph.kefale@gmail.com>
2021-02-16 20:14:09 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,23 +6,15 @@
 */
 void puts_half(char *str)
 {
-char *temp = str;
-char rightHalf[100];
-int length;
-int k;
-int i;
-int mid;
-length = 0;
-while (*temp != '\0')
-{
-length++;
-temp++;
-}
-mid = length/2;
-for (i = mid, k = 0; i <= length; i++, k++)
-{
-_putchar(temp[i]);
-rightHalf[k] = temp[i];
-}
-_putchar(temp[i]);
+    char *temp = str;
+    char rightHalf[100];
+    int length, mid, i, k;
+
+    length = strlen(temp);
+
+    mid = length/2;
+    for(i = mid, k = 0; i <= length; i++, k++) {
+ rightHalf[k]= temp[i];
+      _putchar(rightHalf[k]);
+    }
 }
