90e9c86dc310ca5d594e10d0a9899d3c80bb29ef
livejo <yoseph.kefale@gmail.com>
2021-02-18 16:44:21 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* _strcmp -concatinate string
+*
+*@s1: stores the input
+*@s2: source
+*Return: comp
+*/
+int _strcmp(char *s1, char *s2)
+{
+while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+    {
+        s1++;
+        s2++;
+    }
+
+    if(*s1 == *s2)
+    {
+        return (0); // strings are identical
+    }
+
+    else
+    {
+        return (*s1 - *s2);
+    }
+}
