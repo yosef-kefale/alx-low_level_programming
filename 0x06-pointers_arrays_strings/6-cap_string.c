610dcb765d899e2394efc02ef1ab13e45d8fd993
livejo <yoseph.kefale@gmail.com>
2021-02-18 19:27:19 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+* cap_string -concatinate string
+*
+*@str: stores the input
+*Return: uppercase string
+*/
+char *cap_string(char *)
+{
+char str[MAX]={0};
+	int i;
+	for(i=0; str[i]!='\0'; i++)
+	{
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32;
+			continue;
+		}
+		if(str[i]==' ')
+		{
+			++i;
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32;
+			}
+		}
+		else
+		{
+			if(str[i]>='A' && str[i]<='Z')
+				str[i]=str[i]+32;
+		}
+	}
+	return (str);
+}
