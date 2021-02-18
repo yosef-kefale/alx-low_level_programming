259803b8ad66fb012f6dad47993c9dc25723c1e0
livejo <yoseph.kefale@gmail.com>
2021-02-18 16:19:22 +0300

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _strncat -concatinate string
+*
+*@dest: stores the input
+*@src: source
+*@n: number of characters
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int j;
+i = 0;
+j = 0;
+for (i = 0;i <= n; i++)
+{
+dest[i] = src[j];
+j++;
+}
+return (dest);
+}
