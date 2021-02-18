68bde664ddafd87d9b3ebf049f23041406dfa2d3
livejo <yoseph.kefale@gmail.com>
2021-02-18 16:46:47 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,16 @@
 int _strcmp(char *s1, char *s2)
 {
 while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
-    {
-        s1++;
-        s2++;
-    }
-
-    if(*s1 == *s2)
-    {
-        return (0); // strings are identical
-    }
-
-    else
-    {
-        return (*s1 - *s2);
-    }
+{
+s1++;
+s2++;
+}
+if(*s1 == *s2)
+{
+return (0);
+}
+else
+{
+return (*s1 - *s2);
+}
 }
