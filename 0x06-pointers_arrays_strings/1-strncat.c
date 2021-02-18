e55d424c988595c36c7ba7ddc97f776e9e0b44a7
livejo <yoseph.kefale@gmail.com>
2021-02-18 15:31:08 +0300

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* _strcat -change value using the pointer address
+*
+*@dest: stores the input
+*@src: source
+*Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (j <= n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
