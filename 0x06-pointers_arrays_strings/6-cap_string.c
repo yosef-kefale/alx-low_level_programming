b8c34893cf375bcd9b4c8f8e8912fbc6233dd119
livejo <yoseph.kefale@gmail.com>
2021-02-18 19:43:26 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,29 +7,28 @@
 */
 char *cap_string(char *)
 {
-char str[MAX]={0};
-	int i;
-	for(i=0; str[i]!='\0'; i++)
-	{
-		if(i==0)
-		{
-			if((str[i]>='a' && str[i]<='z'))
-				str[i]=str[i]-32;
-			continue;
-		}
-		if(str[i]==' ')
-		{
-			++i;
-			if(str[i]>='a' && str[i]<='z')
-			{
-				str[i]=str[i]-32;
-			}
-		}
-		else
-		{
-			if(str[i]>='A' && str[i]<='Z')
-				str[i]=str[i]+32;
-		}
-	}
-	return (str);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if((str[i]>='a' && str[i]<='z'))
+str[i]=str[i]-32;
+continue;
+}
+if(str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+++i;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+else
+{
+if (str[i] >= 'A' && str[i] <= 'Z')
+str[i] = str[i] + 32;
+}
+}
+return (str);
 }
