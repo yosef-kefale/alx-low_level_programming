7542d71dc976adecd753d7344cb1dae99bfe8bc7
livejo <yoseph.kefale@gmail.com>
2021-02-18 15:35:55 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 int i;
 int j;
 i = 0;
-j = 1;
+j = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-while (j <= n)
+while (j < n)
 {
 dest[i] = src[j];
 i++;
