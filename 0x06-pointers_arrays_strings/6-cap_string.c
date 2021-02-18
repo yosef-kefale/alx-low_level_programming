999a8d0e780255f16041f232f6d107935e902813
livejo <yoseph.kefale@gmail.com>
2021-02-18 19:52:01 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,8 +16,7 @@ if ((str[i] >= 'a' && str[i] <= 'z'))
 str[i] = str[i] - 32;
 continue;
 }
-if (str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i]
-    == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+if (str[i] == ' ')
 {
 ++i;
 if (str[i] >= 'a' && str[i] <= 'z')
