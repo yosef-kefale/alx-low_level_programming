134af6bb6bb7871f6b226484e7555a9fde8b8b51
livejo <yoseph.kefale@gmail.com>
2021-02-22 16:02:04 +0300

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* _strstr -check character in a string
+*
+*@haystack: source
+*@needle: character
+*Return: number of characters
+*/
+char *_strstr(char *haystack, char *needle)
+{
+char *start;
+for(start = haystack; *start != '\0'; start++)
+{
+char *p = needle;
+char *q = start;
+while ( *p != '\0' && *q != '\0' && *p == *q)
+{ 
+p++;
+q++;
+}
+if (*p == '\0')
+{
+return (start);
+}
+}
+return (\n);
+}
