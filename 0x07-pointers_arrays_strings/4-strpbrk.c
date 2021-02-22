cf268d3d61245e19a9cf46dcfd8658ff09d59d78
livejo <yoseph.kefale@gmail.com>
2021-02-22 15:25:21 +0300

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* _strspn -check character in a string
+*
+*@s: source
+*@accept: character
+*Return: number of characters
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int acceptL;
+int sLength;
+int i;
+int j;
+int k;
+while (s[i] != '\0')
+{
+acceptL++;
+}
+while (accept[i] != '\0')
+{
+sLength++;
+}
+for(i = 0 ; i < sLength; i++)
+{
+for(j = 0; j < acceptL; j++)
+{
+if(s[i] == accept[j])
+{
+for(k = i; s[k]!= '\0'; k++)
+printf("%c",s[k]);
+return (0);
+}
+}
+}
+return (0);
+}
