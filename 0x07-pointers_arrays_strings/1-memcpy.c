d412ada2da4bab5a996d04d60a2ee7b16d77612d
livejo <yoseph.kefale@gmail.com>
2021-02-22 10:58:56 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,11 +7,10 @@
 *@n: number of characters
 *Return: dest
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int index;
 char *memory = src, value = n;
-
 for (index = 0; index < n; index++)
 dest[index] = memory[index];
 return (dest);
