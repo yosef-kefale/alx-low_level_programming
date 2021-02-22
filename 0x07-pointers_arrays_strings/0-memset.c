33d2897856ee3c73ca18e22b890ce90c694b3ced
livejo <yoseph.kefale@gmail.com>
2021-02-22 10:35:48 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,8 +9,8 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int index;
-unsigned char *memory = s, value = b;
+int index;
+char *memory = s, value = b;
 for (index = 0; index < n; index++)
 memory[index] = value;
 return (memory);
