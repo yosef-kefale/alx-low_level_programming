1c7df86cea2ca9e21180de088c697433530b83f4
livejo <yoseph.kefale@gmail.com>
2021-02-24 10:42:24 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,8 +6,9 @@
 */
 int _strlen_recursion(char *s)
 {
- static int length=0;
-    if(*s!=NULL)
+ static int length;
+ length = 0;
+    if(*s != '\0')
     {
         length++;
         _strlen_recursion(++s);
