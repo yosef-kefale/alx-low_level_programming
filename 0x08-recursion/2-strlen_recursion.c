299d83e51e8598e308946c62be43352dbf028c2a
livejo <yoseph.kefale@gmail.com>
2021-02-24 10:29:33 +0300

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _strlen_recursion - reverse using recurssion
+*
+*@s: string
+*/
+int _strlen_recursion(char *s)
+{
+int length;
+if (*s)
+{
+length++;
+_print_rev_recursion(s+1);
+}
+else
+{
+return (length);
+}
+}
