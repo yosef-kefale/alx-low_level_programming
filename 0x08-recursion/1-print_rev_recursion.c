067c3c8b970565d192e80049d43eff0c45098d80
livejo <yoseph.kefale@gmail.com>
2021-02-24 10:19:54 +0300

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - reverse using recurssion
+*
+*@s: string
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s+1);
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+}
+}
