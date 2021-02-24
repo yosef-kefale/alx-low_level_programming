45e25ef42086b4cff0277dced956aefa1dd79a24
livejo <yoseph.kefale@gmail.com>
2021-02-24 11:48:51 +0300

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* is_prime_number -recurssion
+*
+*@n: number
+*/
+int is_prime_number(int n)
+{
+int i;
+i = n / 2;
+ if(i==1)
+    {
+        return (1);
+    }
+    else if(n %i==0)
+    {
+         return (0);
+    }     
+    else
+       {
+         i = i -1; 
+         is_prime_number(n1);
+      }
+}
