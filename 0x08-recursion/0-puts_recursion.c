54844913d66895fbce9d6637eb661cd67d3e93e3
livejo <yoseph.kefale@gmail.com>
2021-02-24 10:14:44 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,16 @@
 * _puts_recursion -recurssion
 *
 *@s: string
-*Return: dest
 */
 void _puts_recursion(char *s)
 {
- if (*s) 
-   { 
-  _putchar(*s);     
-  _puts_recursion(s+1);  
-   } 
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s+1);
+}
+else
+{
+_putchar('\n');
+}
 }
