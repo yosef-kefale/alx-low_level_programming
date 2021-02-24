cd3476c36736edda8bbf1704182a453927f0945a
livejo <yoseph.kefale@gmail.com>
2021-02-24 15:00:36 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,31 @@
 #include "holberton.h"
+
 /**
-* is_prime_number -recurssion
-*
-*@n: number
-*/
+ * prime_check - checks to see if number is prime
+ * @f: factor check
+ * @p: possible prime number
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int prime_check(int f, int p)
+{
+	if (p < 2 || p % f == 0)
+		return (0);
+	else if (f > p / 2)
+		return (1);
+	else
+		return (prime_check(f + 1, p));
+}
+
+/**
+ * is_prime_number - states if number is prime
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 if not
+ */
 int is_prime_number(int n)
 {
-int i;
-i = n / 2;
- if(i==1)
-    {
-        return (1);
-    }
-    else if(n %i==0)
-    {
-         return (0);
-    }     
-    else
-       {
-         i = i -1; 
-         is_prime_number(n);
-      }
- return (0);
+	if (n == 2)
+		return (1);
+	return (prime_check(2, n));
 }
