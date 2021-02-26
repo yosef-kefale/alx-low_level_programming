7a238de6ab2a19d2e0b9148da18114fe5bc6df88
livejo <yoseph.kefale@gmail.com>
2021-02-26 16:01:24 +0000

    new

--- 0x09-static_libraries/_strstr.c
+++ 0x09-static_libraries/_strstr.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
-* _strlen -change value using the pointer address
+*_strstr-change value using the pointer address
 *
 *@s: stores the input
 *Return: length
 */
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
 
 }
