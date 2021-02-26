f06a08ccb2bf0022510df2c56fec19df2d5a8cd6
livejo <yoseph.kefale@gmail.com>
2021-02-26 16:47:06 +0300

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _strlen -change value using the pointer address
+*
+*@s: stores the input
+*Return: length
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
