60af0ac7fe61c522fe4ae55ff3d71b9065230cd9
livejo <yoseph.kefale@gmail.com>
2021-02-26 16:42:03 +0300

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _isalpha - does my stuff
+* @c: user input
+*
+* Return: if its alphanumeric.
+*/
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
+}
