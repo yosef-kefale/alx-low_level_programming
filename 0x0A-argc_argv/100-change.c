2832fd6a538bf0b6b7e0ab69947540e0865a0ab6
livejo <yoseph.kefale@gmail.com>
2021-02-28 15:13:08 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,30 +3,32 @@
 
 int main(int argc, char *argv[])
 {
-int num1,j,counter,result;
-int coin[4];
-coin[0]=25;
-coin[1]=10;
-coin[2]=5;
-coin[3]=2;
-coin[4]=1;
+int num1,j,i,m,counter,result;
+int C[4];
+m = 4; 
+C[0]=25;
+C[1]=10;
+C[2]=5;
+C[3]=2;
+C[4]=1;
 counter = 0;
 if (argc != 2) {
 		printf("Error\n");
 		return(1);
 	}
 else{
-    num1 = atoi(argv[1]);
-    for (j=0;j<=4;j++)
-        {
-            while ((num1 % coin[j])> num1)
-            {
-            result = num1 /coin[j];
-            counter = counter + result;
-            }
-        }
-    }
-	  printf("%d",counter);
+    num = atoi(argv[1]);
+    for(int i = 0; i<=m-1;i++)
+	   {
+	      if(C[i] <= num)
+	      {
+		 int curr_min = minCoin(C, m, num-C[i])
+		 if(curr_min != INT_MAX && curr_min + 1 < min)
+		   min = curr_min + 1
+	      }
+	   }
+  }
+	  printf("%d",min);
   return (0);
 }
 
