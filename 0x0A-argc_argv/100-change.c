f6bbb54fa05a85190ff752a64f82050aaaa13bff
livejo <yoseph.kefale@gmail.com>
2021-02-28 15:31:30 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - prints minimum number of coins to make change for an amount of money
+ * @argc: int
+ * @argv: array of strings
+ * Return: number of coins
+ */
 int main(int argc, char *argv[])
 {
-int num1,j,i,m,counter,result;
-int C[4];
-m = 4; 
-C[0]=25;
-C[1]=10;
-C[2]=5;
-C[3]=2;
-C[4]=1;
-counter = 0;
-if (argc != 2) {
-		printf("Error\n");
-		return(1);
-	}
-else{
-    num = atoi(argv[1]);
-    for(int i = 0; i<=m-1;i++)
-	   {
-	      if(C[i] <= num)
-	      {
-		 int curr_min = minCoin(C, m, num-C[i])
-		 if(curr_min != INT_MAX && curr_min + 1 < min)
-		   min = curr_min + 1
-	      }
-	   }
-  }
-	  printf("%d",min);
-  return (0);
-}
+	int cash;
+	int count;
 
+	count = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cash = atoi(argv[1]);
+	if (cash <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (count = 0; cash > 0; count++)
+	{
+		if (cash >= 25)
+			cash = cash - 25;
+		else if (cash >= 10 && cash < 25)
+			cash = cash - 10;
+		else if (cash >= 5 && cash < 10)
+			cash = cash - 5;
+		else if (cash >= 2 && cash < 5)
+			cash = cash - 2;
+		else
+			cash = cash - 1;
+	}
+	printf("%d\n", count);
+	return (0);
+}
