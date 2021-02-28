5d0d1b6be8df2b5f57e773d49a3424411b22ec04
livejo <yoseph.kefale@gmail.com>
2021-02-28 23:46:25 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints minimum number of coins to make change for an amount of money
+ * main - prints minimum number of coins to change a money
  * @argc: int
- * @argv: array of strings
+ * @arg: array of strings
  * Return: number of coins
  */
 int main(int argc, char *argv[])
 {
-	int cash;
-	int count;
-
-	count = 0;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	cash = atoi(argv[1]);
-	for (cash = 0; cash > 0; cash++)
-	{
-		if (cash > 25)
-			cash = cash - 25;
-		else if (cash > 10 && cash < 25)
-			cash = cash - 10;
-		else if (cash > 5 && cash < 10)
-			cash = cash - 5;
-		else if (cash > 2 && cash < 5)
-			cash = cash - 2;
-		else
-			cash = cash - 1;
-	}
-	printf("%d\n", count);
-	return (0);
+int cash;
+int count;
+count = 0;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cash = atoi(argv[1]);
+for (cash = 0; cash > 0; cash++)
+{
+if (cash > 25)
+cash = cash - 25;
+else if (cash > 10 && cash < 25)
+cash = cash - 10;
+else if (cash > 5 && cash < 10)
+cash = cash - 5;
+else if (cash > 2 && cash < 5)
+cash = cash - 2;
+else
+cash = cash - 1;
+}
+printf("%d\n", count);
+return (0);
 }
