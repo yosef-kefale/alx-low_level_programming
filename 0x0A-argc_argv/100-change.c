b597d7ec2f04689b56b3e9e9532065f03c428701
livejo <yoseph.kefale@gmail.com>
2021-02-28 14:10:23 +0300

    Create 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+int num1, i, j;
+i = 0;
+int coin[4]={25, 10, 5, 2,1};
+int counter;
+counter =0;
+int result;
+if (argc > 2) {
+		printf("Error\n");
+		return(1);
+	}
+else{
+    num1 = atoi(argv[1]);
+    for (j=0;j<=4;j++)
+        {
+            while ((num1 / coin[i])> coin)
+            {
+            num1 = num1 /coin[i];
+            counter ++;
+            }
+        }
+  printf("%d/n",counter);
+    }
+  return (0);
+}
+
