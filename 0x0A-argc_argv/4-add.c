2583b54bcd9a354679fb519d2873a3ffefd1bb57
livejo <yoseph.kefale@gmail.com>
2021-02-28 13:23:26 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define UNUSED(x)(void)(x)
 /**
- * main - multiplies two numbers
- * @argc: int
- * @argv: array of strings
- * Return: sum
+ * main - add arguments which are intigers only
+ * @argc:length of argument
+ * @argv:array of strings
+ * Return: sum for intigers
  */
-int isNumeric(char *argv[], int argc)
-{
-	int i = 0;
-   for (i = 1; i < argc; i++)
-      if (isdigit(argv[i]) == false)
-      return (-1);
-   return (1);
-}
 int main(int argc, char *argv[])
 {
-  int sum;
-  if (isNumeric(char *argv[],int argc) == -1)
-  {
-  printf("Error\n");
-  return (1);
-  }
-  else
-  {
-  for int (i = 1; i < argc; i++)
-  {
-  sum = sum + argv[i];
-  }
-  }
-	printf("%d\n", sum);
-	return (0);
+int i;
+int j;
+int num;
+int sum;
+char *s;
+sum = 0;
+c = 0;
+i = 1;
+while (i < argc)
+{
+s = argv[i];
+j = 0;
+while (s[j] != '\0')
+{
+if (s[j] < '0' || s[j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+j++;
+}
+i++;
+}
+for (i = 1; i < argc; i++)
+{
+num = atoi(argv[i]);
+sum = sum + num;
+}
+printf("%d\n", sum);
+return (0);
 }
