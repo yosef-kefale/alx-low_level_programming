598510ab1e78aa3b76f1b4a178bc69d6f0dd73f3
livejo <yoseph.kefale@gmail.com>
2021-02-28 15:38:26 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,18 +24,18 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (count = 0; cash > 0; count+=cash)
+	for (count = 0; cash > 0; count++)
 	{
 		if (cash >= 25)
-			cash = cash / 25;
+			cash = cash - 25;
 		else if (cash >= 10 && cash < 25)
-			cash = cash / 10;
+			cash = cash - 10;
 		else if (cash >= 5 && cash < 10)
-			cash = cash / 5;
+			cash = cash - 5;
 		else if (cash >= 2 && cash < 5)
-			cash = cash / 2;
+			cash = cash - 2;
 		else
-			cash = cash / 1;
+			cash = cash - 1;
 	}
 	printf("%d\n", count);
 	return (0);
