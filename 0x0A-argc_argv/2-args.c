43b61ff204dc15b9348b5caef511e3fe445dda6d
livejo <yoseph.kefale@gmail.com>
2021-02-28 11:15:08 +0300

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print name of the file
+ * @argc: length of the argument
+ * @argv: string
+ * Return: name of file
+ */
+int main(int argc, char *argv[])
+{
+int i;
+i = 0;
+for int (i = 0;i < argc;i++)
+{
+	printf("%s\n", argv[i]);
+}
+	return (0);
+}
