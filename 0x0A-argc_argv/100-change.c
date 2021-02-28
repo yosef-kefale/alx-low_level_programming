38549045594daf05fe52770ca4da08ce4cbc9b1d
livejo <yoseph.kefale@gmail.com>
2021-02-28 22:54:35 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,13 +26,13 @@ int main(int argc, char *argv[])
 	}
 	for (count = 0; cash > 0; count++)
 	{
-		if (cash >= 25)
+		if (cash > 25)
 			cash = cash - 25;
-		else if (cash >= 10 && cash < 25)
+		else if (cash > 10 && cash < 25)
 			cash = cash - 10;
-		else if (cash >= 5 && cash < 10)
+		else if (cash > 5 && cash < 10)
 			cash = cash - 5;
-		else if (cash >= 2 && cash < 5)
+		else if (cash > 2 && cash < 5)
 			cash = cash - 2;
 		else
 			cash = cash - 1;
