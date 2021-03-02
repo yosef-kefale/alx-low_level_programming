1b3a91158f693ad03494866e9ed74181020cde49
livejo <yoseph.kefale@gmail.com>
2021-03-02 22:36:15 +0300

    Update 5-argstostr.c

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,47 +10,43 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *s;
-	int i;
-	int j;
-	int k;
-	int size;
-
-	size = 0;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	i = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			size++;
-			j++;
-		}
-		i++;
-	}
-	size = size + ac + 1;
-
-	s = malloc(size * sizeof(char));
-	if (s == 0)
-		return (0);
-
-	k = 0;
-	i = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			s[k] = av[i][j];
-			j++;
-			k++;
-		}
-	s[k] = '\n';
-	k++;
-	i++;
+char *s;
+int i;
+int j;
+int k;
+int size;
+size = 0;
+if (ac == 0 || av == NULL)
+return (NULL);
+i = 0;
+while (i < ac)
+{
+j = 0;
+while (av[i][j] != '\0')
+{
+size++;
+j++;
+}
+i++;
+}
+size = size + ac + 1;
+s = malloc(size * sizeof(char));
+if (s == 0)
+return (0);
+k = 0;
+i = 0;
+while (i < ac)
+{
+j = 0;
+while (av[i][j] != '\0')
+{
+s[k] = av[i][j];
+j++;
+k++;
+}
+s[k] = '\n';
+k++;
+i++;
 }
 s[k] = '\0';
 return (s);
