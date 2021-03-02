2d963a5d30c88318a2709d9dc7125ab6d4f37652
livejo <yoseph.kefale@gmail.com>
2021-03-02 20:55:06 +0300

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* create_array - multiplies two numbers
+* @size: int
+* @c: array of strings
+*Return: pointer
+*/
+char *str_concat(char *s1, char *s2)
+{
+{
+int str_size;
+int i;
+int j;
+int k;
+char *dup;
+str_size = 0;
+j = 0;
+k = 0;
+if (str == 0)
+return (0);
+while (s1[str_size] != '\0')
+{
+str_size++;
+}
+while (s2[j] != '\0')
+{
+str_size++;
+j++;
+}
+str_size++;
+dup = malloc(str_size *sizeof(char));
+if (dup == 0)
+return (0);
+i = 0;
+while (str[i] != '\0')
+{
+dup[i] = s1[i];
+i++;
+}
+while (s2[k] != '\0')
+{
+dup[i] = s1[k];
+i++;
+k++;
+}
+dup[i] = '\0';
+return (dup);
+}
