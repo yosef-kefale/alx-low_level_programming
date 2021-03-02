2a06019de52d8918a117e30eb932c7206e15a2b2
livejo <yoseph.kefale@gmail.com>
2021-03-02 11:55:27 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
+  char *str;
 unsigned int i;
 if (size == 0)
 {
 return (NULL);
 }
- char *str;
  str = malloc((size) * sizeof(char));
  for (i=0;i<size;i++)
  {
