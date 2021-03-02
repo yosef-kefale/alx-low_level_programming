e8812f0fbaddacd87137e250c1d14b4effcde159
livejo <yoseph.kefale@gmail.com>
2021-03-02 14:45:10 +0300

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - multiplies two numbers
+ * Return: pointer 
+ */
+char *_strdup(char *str)
+{
+ int str_size;
+ static char *dup;
+ char *dup_offset;
+
+    str_size = strlen(str);
+    dup = (char *)malloc(sizeof(char)*str_size+1);
+    if( dup == NULL)
+        return( (char *)NULL);
+
+    dup_offset = dup;
+    while(*str)
+    {
+        *dup_offset = *str;
+        dup_offset++;
+        org++;
+    }
+    *dup_offset = '\0';
+
+    return(dup);
+}
