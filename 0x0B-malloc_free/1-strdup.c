dc24f1b53b3b6cf721a65ae0db88293d654d59f3
livejo <yoseph.kefale@gmail.com>
2021-03-02 14:50:06 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,28 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strdup - multiplies two numbers
- * Return: pointer 
- */
+* _strdup - point new allocated pointer
+* Return: pointer 
+*/
 char *_strdup(char *str)
 {
- int str_size;
- static char *dup;
- char *dup_offset;
-
-    str_size = strlen(str);
-    dup = (char *)malloc(sizeof(char)*str_size+1);
-    if( dup == NULL)
-        return( (char *)NULL);
-
-    dup_offset = dup;
-    while(*str)
-    {
-        *dup_offset = *str;
-        dup_offset++;
-        str++;
-    }
-    *dup_offset = '\0';
-
-    return(dup);
+int str_size;
+static char *dup;
+char *dup_offset;
+str_size = strlen(str);
+dup = (char *)malloc(sizeof(char)*str_size+1);
+if( dup == NULL)
+return( (char *)NULL);
+dup_offset = dup;
+while(*str)
+{
+*dup_offset = *str;
+dup_offset++;
+str++;
+}
+*dup_offset = '\0';
+return(dup);
 }
