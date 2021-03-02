d7a7dd77c0bf3e6d73eec91ee78d977b1e225eb0
livejo <yoseph.kefale@gmail.com>
2021-03-03 00:00:27 +0300

    Update 100-strtow.c

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -30,69 +30,66 @@ int word_count(char *s)
  */
 char **strtow(char *str)
 {
-	int i, j, k, m, words;
-	char **s;
-
-	if (str == NULL || str == '\0')
-		return (NULL);
-
-	words = word_count(str);
-
-	s = malloc((words + 1) * sizeof(char *));
-	if (s == NULL)
-		return (NULL);
-	i = 0;
-	m = 0;
-	while (i < words)
-	{
-		j = m;
-		while (str[j] != '\0')
-		{
-			if (str[j] != ' ' && str[j + 1] == ' ')
-			{
-				j = m + 1;
-				break;
-			}
-			j++;
-		}
-		s[i] = malloc(j * sizeof(char));
-		if (s[i] == NULL)
-		{
-			while (i >= 0)
-			{
-				free(s[i]);
-				i--;
-			}
-			free(s);
-		}
-		i++;
-	}
-	i = 0;
-	j = 0;
-	if (str[0] == ' ')
-		j++;
-	while (i < words)
-	{
-		k = 0;
-		while (str[j] != '\0')
-		{
-			if (str[j] != ' ')
-			{
-				s[i][k] = str[j];
-				j++;
-				k++;
-			}
-			else if (str[j] == ' ' && str[j - 1] != ' ')
-			{
-				j++;
-				k++;
-				break;
-			}
-			else
-				j++;
-		}
-		s[i][k] = '\0';
-		i++;
-	}
-	return (s);
+int i, j, k, m, words;
+char **s;
+if (str == NULL || str == '\0')
+return (NULL);
+words = word_count(str);
+s = malloc((words + 1) * sizeof(char *));
+if (s == NULL)
+return (NULL);
+i = 0;
+m = 0;
+while (i < words)
+{
+j = m;
+while (str[j] != '\0')
+{
+if (str[j] != ' ' && str[j + 1] == ' ')
+{
+j = m + 1;
+break;
+}
+j++;
+}
+s[i] = malloc(j * sizeof(char));
+if (s[i] == NULL)
+{
+while (i >= 0)
+{
+free(s[i]);
+i--;
+}
+free(s);
+}
+i++;
+}
+i = 0;
+j = 0;
+if (str[0] == ' ')
+j++;
+while (i < words)
+{
+k = 0;
+while (str[j] != '\0')
+{
+if (str[j] != ' ')
+{
+s[i][k] = str[j];
+j++;
+k++;
+}
+else if (str[j] == ' ' && str[j - 1] != ' ')
+{
+j++;
+k++;
+break;
+}
+else
+j++;
+}
+s[i][k] = '\0';
+i++;
+}
+return (s);
 }
