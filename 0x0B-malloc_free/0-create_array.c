339464e56155343a4ebb3e02c1c7ee9d6442ba82
livejo <yoseph.kefale@gmail.com>
2021-03-02 15:33:33 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * create_array - multiplies two numbers
- * @size: int
- * @c: array of strings
- * Return: pointer 
- */
+* create_array - multiplies two numbers
+* @size: int
+* @c: array of strings
+* Return: pointer 
+*/
 char *create_array(unsigned int size, char c)
 {
-  char *str;
+char *str;
 unsigned int i;
 if (size == 0)
 {
 return (NULL);
 }
- str = malloc((size) * sizeof(char));
- for (i=0;i<size;i++)
- {
- str[i] = c;
- }
+str = malloc((size) * sizeof(char));
+for (i = 0; i < size; i++)
+{
+str[i] = c;
+}
 return (str);
 return (0);
 }
