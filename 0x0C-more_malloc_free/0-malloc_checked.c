3e4048f82f0a1880076cb5450a43e975267bffcb
livejo <yoseph.kefale@gmail.com>
2021-03-04 18:54:23 +0300

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* malloc_checked - crete memory allocation
+* @b: int
+*Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
