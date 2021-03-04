c62fed788d1c6631a7c449e32c6ea0305dad3c88
livejo <yoseph.kefale@gmail.com>
2021-03-04 19:08:01 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,49 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
+ * string_nconcat - concatenates 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes from s2 to be concatenated
+ * Return: new array of combined string
  */
-
-int _strlen(char *s)
-{
-int i;
-i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
-}
-/**
-*string_nconcat - change value using
-*@s1: manipulates char1
-*@s2: manipulates char2
-*@n: manipulates integer
-*Return: memory
-*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int a, b, i;
-char *p;
-if (!s1)
-s1 = "";
-else
-a = _strlen(s1);
-if (!s2)
-s2 = "";
-else
-b = _strlen(s2);
-if (n > b)
-n = b;
-p = malloc((a + n + 1) * sizeof(char));
-if (p == NULL)
-return (NULL);
-for (i = 0; i < a; i++)
-{
-p[i] = s1[i];
-}
-for (; i < (a + n); i++)
-{
-p[i] = s2[i - a];
-}
-p[i] = '\0';
-return (p);
+	char *combine;
+	unsigned int len1;
+	unsigned int len2;
+	unsigned int i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+	combine = malloc((len1 + n + 1) * sizeof(char));
+	if (combine == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		combine[i] = s1[i];
+		i++;
+	}
+
+	i = 0;
+	while (i < n)
+	{
+		combine[len1 + i] = s2[i];
+		i++;
+	}
+	combine[len1 + i] = '\0';
+	return (combine);
 }
