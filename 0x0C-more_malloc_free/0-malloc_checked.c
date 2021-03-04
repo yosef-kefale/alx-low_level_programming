f7df42354dcbe62b83e6d30def02a0023aa03fb4
livejo <yoseph.kefale@gmail.com>
2021-03-04 19:01:19 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,21 @@
+
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /**
-* malloc_checked - crete memory allocation
-* @b: int
-*Return: pointer
-*/
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ *
+ * Return: pointer to allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-void *p;
-p = malloc(b);
-if (p == NULL)
-exit(98);
-return (p);
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+
+	return (s);
 }
