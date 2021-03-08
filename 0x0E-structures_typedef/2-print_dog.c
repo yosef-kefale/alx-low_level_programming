e46518a4acf5eb5d6033af41cfe1b2839e7cef38
livejo <yoseph.kefale@gmail.com>
2021-03-08 18:32:42 +0300

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize struct
+ * @d: pointer to dog
+ *
+ * Return: pointer to dog
+ */
+void print_dog(struct dog *d)
+{
+if (dog->name == NULL)
+printf("Name: (nil)");
+else
+printf("Name: %s",dog->name);
+if (dog->age == NULL)
+printf("Age: (nil)");
+else
+printf("Age: %d",dog->age);
+if (dog->owner == NULL)
+printf("Owner: (nil)");
+else
+printf("Owner: %s",dog->owner);
+}
