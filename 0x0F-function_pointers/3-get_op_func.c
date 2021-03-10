d04003a18b1fad10e0ea20084fffe3e6c48f0801
livejo <yoseph.kefale@gmail.com>
2021-03-10 16:12:59 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
-* get_op_func - get operation for use in 3-main
-* @s: char pointer s
-*
-* Return: int
-*/
+ * get_op_func - get an operation
+ * @s: operator
+ * Return: No
+ */
 int (*get_op_func(char *s))(int, int)
 {
-int i;
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-i = 0;
-while (ops[i].f != NULL && strcmp(ops[i].op, s) != 0)
-i++;
-
-return (ops[i].f);
+	while (ops[i].op)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
 }
