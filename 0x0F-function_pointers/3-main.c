c07ed67a6eee832548b51ad3b1e3f9f2fc7445f5
livejo <yoseph.kefale@gmail.com>
2021-03-10 16:13:21 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
+
 /**
-* main - basic calculator
-* @argc: integer
-* @argv: char array
-* Return: result
-*/
+ * main - main file
+ * @argc: number of lines arguments
+ * @argv: array of elements
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-int arg1;
-int arg2;
-int (*oper)(int, int);
-int result;
+	int R;
 
-oper = get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", R);
 
-arg1 = atoi(argv[1]);
-arg2 = atoi(argv[3]);
-
-if (oper == NULL)
-{
-printf("Error\n");
-exit(99);
-}
-
-if ((*argv[2] == '/' || *argv[2] == '%') && arg2 == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
-result = oper(arg1, arg2);
-printf("%d\n", result);
-return (0);
+	return (0);
 }
