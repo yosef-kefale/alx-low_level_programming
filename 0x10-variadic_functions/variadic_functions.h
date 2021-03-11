6fe057d612cf17c1311740c7a172b679dede460e
livejo <yoseph.kefale@gmail.com>
2021-03-11 22:43:43 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
+#ifndef VARDIAC_H
+#define VARDIAC_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
