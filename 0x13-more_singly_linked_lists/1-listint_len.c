9f6d004236bec88f6e319e41ec7a99aeee065d32
livejo <yoseph.kefale@gmail.com>
2021-03-23 13:46:45 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,11 +7,10 @@
 size_t listint_len(const listint_t *h);
 {
 size_t count = 0;
-size_t listint_t* current = h;
-while (current != NULL) 
+while (h) 
 { 
-count++; 
-current = current->next; 
+h = h->next;
+count++;  
 } 
 return (count);
 }
