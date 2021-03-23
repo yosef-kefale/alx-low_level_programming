8fa89d8fe47c512465fc618c092e12a600121917
livejo <yoseph.kefale@gmail.com>
2021-03-23 13:48:58 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * listint_len - count elements of listint
- * @h: pointer to first node of list
- * Return: count of elements
- */
+* listint_len - prints n count n list
+* @h: pointer to first
+*Return: pointer
+*/
 size_t listint_len(const listint_t *h);
 {
 size_t count = 0;
@@ -13,4 +13,5 @@ h = h->next;
 count++;  
 } 
 return (count);
+}
 }
