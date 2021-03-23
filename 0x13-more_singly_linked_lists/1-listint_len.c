2912143cceb320cab5dbfa9ef03049268108b5fe
livejo <yoseph.kefale@gmail.com>
2021-03-23 13:45:36 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,12 +6,12 @@
  */
 size_t listint_len(const listint_t *h);
 {
- size_t count = 0;
-    struct listint_t* current = h;
-    while (current != NULL) 
-    { 
-        count++; 
-        current = current->next; 
-    } 
-    return count;
+size_t count = 0;
+size_t listint_t* current = h;
+while (current != NULL) 
+{ 
+count++; 
+current = current->next; 
+} 
+return (count);
 }
