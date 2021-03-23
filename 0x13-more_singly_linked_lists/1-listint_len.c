ed5913ac0b4051adc0e6a85d3c5e2d07cbc6c1a1
livejo <yoseph.kefale@gmail.com>
2021-03-23 13:52:38 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,15 +2,15 @@
 /**
 * listint_len - prints n count n list
 * @h: pointer to first
-*Return: pointer
+*Return: return number of elements in a linked list
 */
 size_t listint_len(const listint_t *h)
 {
-size_t i = 0;
+size_t count = 0;
 while (h)
 {
 h = h->next;
-i++;
+count++;
 }
-return (i);
+return (count);
 }
