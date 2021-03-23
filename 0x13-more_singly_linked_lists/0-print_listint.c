1a510da678c301516d9bda03dcd4050bb970e91e
livejo <yoseph.kefale@gmail.com>
2021-03-23 13:29:59 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 /**
-* print_listint - prints n count n list
-* @h: pointer to first
-*Return: pointer
-*/
+ * print_listint - prints all elements of listint
+ * @h: pointer to first node of list
+ * Return: num of elements
+ */
 size_t print_listint(const listint_t *h)
 {
-size_t i = 0;
-while (h)
-{
-printf("%i\n", h->n);
-h = h->next;
-i++;
-}
-return (i);
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
