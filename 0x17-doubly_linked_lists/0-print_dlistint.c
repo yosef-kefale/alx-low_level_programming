ef0d65bdfe98be238fbcae514e2bbf05f4e796c5
livejo <yoseph.kefale@gmail.com>
2021-05-02 05:46:39 +0300

    Create 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of dlistint
+ * @h: pointer to first node of list
+ * Return: num of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    struct dlistint_s *current = h;  
+    if(h == NULL) {  
+        return;  
+    }   
+    while(current != NULL) {  
+        printf("%d ", current->n);  
+        current = current->next;  
+    } 
+}
