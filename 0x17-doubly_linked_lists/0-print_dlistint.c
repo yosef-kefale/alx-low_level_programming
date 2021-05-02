46b34f7af1a9bf807fec6d0fd1e3593d6e9b177c
livejo <yoseph.kefale@gmail.com>
2021-05-02 05:59:35 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,12 +6,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-int i;
-i=0;
-    while(h != NULL) {  
-        printf("%d \n", h->n);  
-        h = h->next;
-        i++;
-    } 
-    return (i);
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
